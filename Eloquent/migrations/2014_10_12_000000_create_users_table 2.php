14c56939b305a8edfbc2e8f79b89d367cc875718
Gus <yo@soygus.com>
2017-07-06 12:03:56 -0500

    Adds more migrations and documents.scss

--- Eloquent/migrations/2014_10_12_000000_create_users_table 2.php
+++ Eloquent/migrations/2014_10_12_000000_create_users_table 2.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Model\LuUserRole;
 
 class CreateUsersTable extends Migration
 {
+    const CLIENT_ROLE_ID = 3;
     /**
      * Run the migrations.
      *
@@ -15,6 +17,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('role_id')->length(2)->unsigned()->default(self::CLIENT_ROLE_ID)->comment('FK references lu_user_roles table. The role assigned to the user.');
             $table->string('email')->unique();
             $table->string('name')->nullable();
             $table->string('paternal_last_name')->nullable();
@@ -24,12 +27,21 @@ class CreateUsersTable extends Migration
             $table->date('dob')->nullable();
             $table->string('token', 30)->nullable();
             $table->integer('is_verified')->length(1)->default(0);
-            $table->string('referral_code', 7);
+            $table->string('referral_code', 7)->nullable();
             $table->integer('referred_by')->unsigned()->nullable();
-            $table->integer('role_id')->length(2)->unsigned()->comment('FK references lu_user_roles table. The role assigned to the user.');
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
     }
 }
---
+++ Eloquent/migrations/2017_07_05_143234_create_rel_products_attributes.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRelProductsAttributes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rel_products_attributes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->integer('attribute_id')->unsigned();
+            $table->integer('subattribute_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('rel_products_attributes', function (Blueprint $table) {
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('attribute_id')->references('id')->on('lu_product_attributes');
+            $table->foreign('subattribute_id')->references('id')->on('lu_product_subattributes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rel_products_attributes', function (Blueprint $table) {
+            $table->dropForeign(['attribute_id']);
+        });
+        Schema::table('rel_products_attributes', function (Blueprint $table) {
+            $table->dropForeign(['subattribute_id']);
+        });
+        Schema::table('rel_products_attributes', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+        });
+
+        Schema::dropIfExists('rel_products_attributes');
+    }
+}
